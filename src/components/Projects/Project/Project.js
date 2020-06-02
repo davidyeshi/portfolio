6a04dd3d8e83a028f44c0868002b59caa75e28be
David Yeshi <davidyeshi0@gmail.com>
2020-06-02 19:19:40 +1000

    Added Modal to Project

--- src/components/Projects/Project/Project.js
+++ src/components/Projects/Project/Project.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import {Card, Button} from 'react-bootstrap';
+import React, { Component } from 'react';
+import {Card, Button, Modal} from 'react-bootstrap';
 import Radium from 'radium';
 
 const style = {
     'width': '10rem',
+    'textAlign': 'center',
     '@media (min-width: 500px)': {
         'width': '15rem'
-    }
+    },
+    'margin':'5px'
 };
 
-const project = (props) => (
-    <div style={style}>
-        <Card >
-            <Card.Img variant="top" src={props.projectImage}/>
-            <Card.Body>
-                <Card.Title>{props.title}</Card.Title>
-                <Card.Text>
-                    {props.desc}
-                </Card.Text>
-                <Button variant="primary">{props.link}</Button>
-            </Card.Body>
-        </Card>
-    </div>
-)
+class Project extends Component {
+    
+    state = {
+        show: false
+    }
 
-export default Radium(project);
+    showModal = () => this.setState({show:true});
+    closeModal = () => this.setState({show:false});
+
+    render() {
+        return(
+            <div style={style}>
+                <Card >
+                    <Card.Img variant="top" src={this.props.projectImage}/>
+                    <Card.Header><Button variant="dark" onClick={this.showModal}>View</Button></Card.Header>
+                    <Modal 
+                        show={this.state.show} 
+                        onHide={this.closeModal} 
+                        centered>
+                        <Modal.Header closeButton>
+                            <Modal.Title>{this.props.title}</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>{this.props.desc}</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={this.closeModal}>Close</Button>
+                            <Button variant="dark">{this.props.link}</Button>
+                        </Modal.Footer>
+                    </Modal>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Radium(Project);
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import imgPro from '../../yeshi-5.jpg';
 import Project from './Project/Project';
+import classes from './Projects.module.css';
+
 class Projects extends Component {
     render () {
         return (
-                <Project projectImage={imgPro} title="Some title" desc = "Project Description" link="Project link"  />
+            <div className={classes.Projects}>
+                <Project projectImage={imgPro} eventKey="0" title="Some title" desc = "Project DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject Description" link="Project link"  />
+                <Project projectImage={imgPro} eventKey="1" title="Some title" desc = "Project DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject Description" link="Project link"  />
+                <Project projectImage={imgPro} eventKey="2" title="Some title" desc = "Project DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject Description" link="Project link"  />
+                <Project projectImage={imgPro} eventKey="3" title="Some title" desc = "Project DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject Description" link="Project link"  />
+                <Project projectImage={imgPro} eventKey="4" title="Some title" desc = "Project DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject Description" link="Project link"  />
+                <Project projectImage={imgPro} eventKey="5" title="Some title" desc = "Project DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject DescriptionProject Description" link="Project link"  />
+            </div>
         );
     }
 }
